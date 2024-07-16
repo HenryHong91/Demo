519df017d7366d92d5e9960ee5216925cee29c48
henryHongStudent <lvsy91@gmail.com>
2024-07-16 14:09:09 +1200

    Demo Ver

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/UI-UX-design/",
+  base: "/Demo/",
   plugins: [react()],
 });
